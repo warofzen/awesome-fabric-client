9e8d050ba4088a11e727e55805703fb49ce1c0e1
wrham <wrham001@gmail.com>
2021-12-09 15:00:10 +0700

    1.18 version and fixes 1.17.1 version

---
+++ 1.18/config/slightguimodifications/cts.groovy
@@ -0,0 +1,15 @@
+mainMenu {
+    enabled = true
+    label {
+        position {
+            x { it - 2 }
+            y { it - 20 }
+        }
+        text = literal("Awesome Fabric CLient 2.0")
+        align = "right"
+        color = 0xFFFFFF
+        hoveredColor = 0x55FFFF
+        shadow = true
+        onClicked = nothing()
+    }
+}
