9287410a22c0b09d198cc60e2d8a65e79d02c88d
wrham <wrham001@gmail.com>
2021-12-16 16:53:18 +0700

    2.2

--- 1.18.1/config/slightguimodifications/cts.groovy
+++ 1.18.1/config/slightguimodifications/cts.groovy
@@ -5,7 +5,7 @@ mainMenu {
             x { it - 2 }
             y { it - 20 }
         }
-        text = literal("Awesome Fabric CLient 2.0")
+        text = literal("Awesome Fabric Client 2.2")
         align = "right"
         color = 0xFFFFFF
         hoveredColor = 0x55FFFF
