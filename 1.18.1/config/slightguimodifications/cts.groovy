fb43ea7e5c43d93853989c8e1d6dd5bd55bdb154
wrham <wrham001@gmail.com>
2021-12-20 07:48:18 +0700

    2.2.1

--- 1.18.1/config/slightguimodifications/cts.groovy
+++ 1.18.1/config/slightguimodifications/cts.groovy
@@ -5,7 +5,7 @@ mainMenu {
             x { it - 2 }
             y { it - 20 }
         }
-        text = literal("Awesome Fabric Client 2.2")
+        text = literal("Awesome Fabric Client 2.2.1")
         align = "right"
         color = 0xFFFFFF
         hoveredColor = 0x55FFFF
---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Awesome Fabric Client Changelog
+
+## 1.18.x
+
+### 2.2.1
+
+* Add fixed options txt
+* Updated Mods: Architectury API, Clumps, Nicephore
